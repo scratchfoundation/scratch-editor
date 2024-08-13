29bd8bfbda4d3e38cac48fa7475229a552200d0c
Georgi Angelov <gangelov@asteasolutions.com>
2024-08-13 15:11:59 +0300

    chore: format prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  semi: true,
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    semi: true,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
 
-  plugins: [
-    require.resolve('@trivago/prettier-plugin-sort-imports'),
-    'prettier-plugin-tailwindcss',
-  ],
-  importOrder: ['^@', '^[./]'],
-  importOrderSeparation: false,
-}
+    plugins: [
+        require.resolve('@trivago/prettier-plugin-sort-imports'),
+        'prettier-plugin-tailwindcss',
+    ],
+    importOrder: ['^@', '^[./]'],
+    importOrderSeparation: false,
+};
