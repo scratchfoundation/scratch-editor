8ae91941789afe2750fa5df6cb1a5d5878f89176
Georgi Angelov <gangelov@asteasolutions.com>
2024-08-13 14:44:28 +0300

    chore: add scratch-web project with minimal prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  plugins: [
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: ['^@', '^[./]'],
+  importOrderSeparation: false,
+}
