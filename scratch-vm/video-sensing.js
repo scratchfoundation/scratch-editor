dae4438a574a8b0e027e5a7f1f67c744853e4569
KManolov3 <KManolov3@users.noreply.github.com>
2025-07-31 14:40:13 +0000

    Build for b173a6e2eedc9c72bee11b060d96391a16e6a45d

--- scratch-vm/video-sensing.js
+++ scratch-vm/video-sensing.js
@@ -1,1 +1,133 @@
-!function(){const e=document.createElement("video");navigator.getUserMedia({audio:!1,video:{width:{min:480,ideal:640},height:{min:360,ideal:480}}},t=>{e.autoplay=!0,e.src=window.URL.createObjectURL(t),t.getTracks(),e.addEventListener("play",()=>{e.width=e.videoWidth,e.height=e.videoHeight})},e=>{console.log(e)});const t=window.Scratch3VideoSensingDebug.VideoMotion,n=window.Scratch3VideoSensingDebug.VideoMotionView,o=new t,i=n.OUTPUT,c=Object.keys(i),a=Object.values(i),d=a.map(e=>new n(o,e)),h=d[0],r=[i.INPUT,i.XY_CELL,i.T_CELL,i.UV_CELL],s=document.createElement("div");s.style.userSelect="none",a.forEach((e,t)=>{const n=document.createElement("label"),o=document.createElement("input");o.type="checkbox",o.checked=-1!==r.indexOf(e);const i=document.createElement("span");i.innerText=c[t],n.appendChild(o),n.appendChild(i);const a=d[t];a.canvas.style.display=o.checked?"":"none",a.active=o.checked,o.onchange=e=>(a.canvas.style.display=o.checked?"":"none",a.active=o.checked,e.preventDefault(),!1),s.appendChild(n)}),document.body.appendChild(s);const l=document.createElement("div"),m=document.createElement("div");m.innerText="duration (us) :: motion amount :: motion direction",l.appendChild(m);const u=document.createElement("div");u.innerText="0 :: 0 :: 0",l.appendChild(u),document.body.appendChild(l);let p=Date.now();d.forEach(e=>document.body.appendChild(e.canvas));const w=document.createElement("canvas");w.width=h.canvas.width,w.height=h.canvas.height;const g=w.getContext("2d"),v=function(){const t=setTimeout(v,33);try{g.scale(-1,1),g.drawImage(e,0,0,e.width||e.clientWidth,e.height||e.clientHeight,-w.width,0,w.width,w.height),g.resetTransform();const t=g.getImageData(0,0,w.width,w.height),n=performance.now();if(o.addFrame(t.data),o.analyzeFrame(),Date.now()-p>250){const e=(1e3*(performance.now()-n)).toFixed(0),t=o.motionAmount.toFixed(1),i=o.motionDirection.toFixed(1);u.innerText=`${e} :: ${t} :: ${i}`,p=Date.now()}d.forEach(e=>e.active&&e.draw())}catch(e){console.error(e.stack||e),clearTimeout(t)}};v()}();
+(function () {
+    const BENCHMARK_THROTTLE = 250;
+    const INTERVAL = 33;
+
+    const video = document.createElement('video');
+    navigator.getUserMedia({
+        audio: false,
+        video: {
+            width: {min: 480, ideal: 640},
+            height: {min: 360, ideal: 480}
+        }
+    }, stream => {
+        video.autoplay = true;
+        video.src = window.URL.createObjectURL(stream);
+        // Get the track to hint to the browser the stream needs to be running
+        // even though we don't add the video tag to the DOM.
+        stream.getTracks();
+        video.addEventListener('play', () => {
+            video.width = video.videoWidth;
+            video.height = video.videoHeight;
+        });
+    }, err => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+    });
+
+    const VideoMotion = window.Scratch3VideoSensingDebug.VideoMotion;
+    const VideoMotionView = window.Scratch3VideoSensingDebug.VideoMotionView;
+
+    // Create motion detector
+    const motion = new VideoMotion();
+
+    // Create debug views that will render different slices of how the detector
+    // uses a frame of input.
+    const OUTPUT = VideoMotionView.OUTPUT;
+    const outputKeys = Object.keys(OUTPUT);
+    const outputValues = Object.values(OUTPUT);
+    const views = outputValues
+        .map(output => new VideoMotionView(motion, output));
+    const view = views[0];
+
+    const defaultViews = [OUTPUT.INPUT, OUTPUT.XY_CELL, OUTPUT.T_CELL, OUTPUT.UV_CELL];
+
+    // Add activation toggles for each debug view.
+    const activators = document.createElement('div');
+    activators.style.userSelect = 'none';
+    outputValues.forEach((output, index) => {
+        const checkboxLabel = document.createElement('label');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = defaultViews.indexOf(output) !== -1;
+        const checkboxSpan = document.createElement('span');
+        checkboxSpan.innerText = outputKeys[index];
+        checkboxLabel.appendChild(checkbox);
+        checkboxLabel.appendChild(checkboxSpan);
+
+        const _view = views[index];
+        _view.canvas.style.display = checkbox.checked ? '' : 'none';
+        _view.active = checkbox.checked;
+        checkbox.onchange = event => {
+            _view.canvas.style.display = checkbox.checked ? '' : 'none';
+            _view.active = checkbox.checked;
+            event.preventDefault();
+            return false;
+        };
+
+        activators.appendChild(checkboxLabel);
+    });
+    document.body.appendChild(activators);
+
+    // Add a text line to display milliseconds per frame, motion value, and
+    // motion direction
+    const textContainer = document.createElement('div');
+    const textHeader = document.createElement('div');
+    textHeader.innerText = 'duration (us) :: motion amount :: motion direction';
+    textContainer.appendChild(textHeader);
+    const textEl = document.createElement('div');
+    textEl.innerText = `0 :: 0 :: 0`;
+    textContainer.appendChild(textEl);
+    document.body.appendChild(textContainer);
+    let textTimer = Date.now();
+
+    // Add the motion debug views to the dom after the text line, so the text
+    // appears first.
+    views.forEach(_view => document.body.appendChild(_view.canvas));
+
+    // Create a temporary canvas the video will be drawn to so the video's
+    // bitmap data can be transformed into a TypeArray.
+    const tempCanvas = document.createElement('canvas');
+    tempCanvas.width = view.canvas.width;
+    tempCanvas.height = view.canvas.height;
+    const ctx = tempCanvas.getContext('2d');
+
+    const loop = function () {
+        const timeoutId = setTimeout(loop, INTERVAL);
+
+        try {
+            // Get the bitmap data for the video frame
+            ctx.scale(-1, 1);
+            ctx.drawImage(
+                video,
+                0, 0, video.width || video.clientWidth, video.height || video.clientHeight,
+                -tempCanvas.width, 0, tempCanvas.width, tempCanvas.height
+            );
+            ctx.resetTransform();
+            const data = ctx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+
+            // Analyze the latest frame.
+            const b = performance.now();
+            motion.addFrame(data.data);
+            motion.analyzeFrame();
+
+            // Every so often update the visible debug numbers with duration in
+            // microseconds, the amount of motion and the direction of the
+            // motion.
+            if (Date.now() - textTimer > BENCHMARK_THROTTLE) {
+                const e = performance.now();
+                const analyzeDuration = ((e - b) * 1000).toFixed(0);
+                const motionAmount = motion.motionAmount.toFixed(1);
+                const motionDirection = motion.motionDirection.toFixed(1);
+                textEl.innerText = `${analyzeDuration} :: ${motionAmount} :: ${motionDirection}`;
+                textTimer = Date.now();
+            }
+            views.forEach(_view => _view.active && _view.draw());
+        } catch (error) {
+            // eslint-disable-next-line no-console
+            console.error(error.stack || error);
+            clearTimeout(timeoutId);
+        }
+    };
+
+    loop();
+}());
