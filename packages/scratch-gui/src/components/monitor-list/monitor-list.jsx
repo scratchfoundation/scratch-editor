078452d2fb013b3d378260c1b6bd81a2268be8a7
nimeratus <nemcsics.benedek@gmail.com>
2025-09-07 13:21:16 +0200

    fix: dragging bounds
    Use the CSS transform to resize the dragging bounds,
    instead of changing the width/height properties.
    This way CSS word-wrapping and react-draggable will
    let the monitors go to the edges of the stage.

--- packages/scratch-gui/src/components/monitor-list/monitor-list.jsx
+++ packages/scratch-gui/src/components/monitor-list/monitor-list.jsx
@@ -11,15 +11,14 @@ import styles from './monitor-list.css';
 const MonitorList = props => (
     <Box
         // Use static `monitor-overlay` class for bounds of draggables
-        className={classNames(styles.monitorList, 'monitor-overlay')}
+        className={classNames(styles.monitorList, styles.monitorListScaler, 'monitor-overlay')}
         style={{
-            width: props.stageSize.width,
-            height: props.stageSize.height
+            width: props.stageSize.widthDefault,
+            height: props.stageSize.heightDefault,
+            ...stageSizeToTransform(props.stageSize)
         }}
     >
         <Box
-            className={styles.monitorListScaler}
-            style={stageSizeToTransform(props.stageSize)}
         >
             {props.monitors.valueSeq().filter(m => m.visible)
                 .map(monitorData => (
