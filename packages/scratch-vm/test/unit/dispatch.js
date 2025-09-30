b8680f7acfc23095f06d8430dfc6ebcbe7aef34b
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-09-30 13:10:07 -0700

    test: more tests for dispatch exception handling

--- packages/scratch-vm/test/unit/dispatch.js
+++ packages/scratch-vm/test/unit/dispatch.js
@@ -9,10 +9,41 @@ const dispatch = require('../../src/dispatch/central-dispatch');
 
 const DispatchTestService = require('../fixtures/dispatch-test-service');
 
+/**
+ * @typedef {Parameters<Parameters<typeof tap.test>[0]>[0]} Test
+ * The type of the test object passed to a `tap` test callback.
+ *
+ * TODO: Is there a better way to import this type?
+ */
+
 const {test} = tap;
 
+/**
+ * Run tests for a specific dispatch service.
+ * @param {string} serviceName - name of the service to test
+ * @param {Test} t - tap test object
+ * @returns {Promise<any>} - promise which will resolve/reject based on test results
+ */
 const runServiceTest = function (serviceName, t) {
     const promises = [];
+
+    promises.push(dispatch.call('invalidServiceName', 'invalidMethodName')
+        .then(
+            () => t.fail('call to invalid service did not fail as expected'),
+            e => {
+                t.type(e, Error);
+                t.equal(e.message, 'Service not found: invalidServiceName');
+            }
+        ));
+
+    promises.push(dispatch.call(serviceName, 'invalidMethodName')
+        .then(
+            () => t.fail('call to invalid method did not fail as expected'),
+            e => {
+                t.type(e, Error);
+                t.equal(e.message, 'Method not found on this service: invalidMethodName');
+            }
+        ));
 
     promises.push(dispatch.call(serviceName, 'returnFortyTwo')
         .then(
@@ -36,7 +67,10 @@ const runServiceTest = function (serviceName, t) {
     promises.push(dispatch.call(serviceName, 'throwException')
         .then(
             () => t.fail('exception was not propagated as expected'),
-            () => t.pass('exception was propagated as expected')
+            e => {
+                t.type(e, Error);
+                t.equal(e.message, 'This is a test exception thrown by DispatchTest');
+            }
         ));
 
     return Promise.all(promises);
