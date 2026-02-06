bf0c57f29f9ed44dcc69faa3ea5bdebbe1693f80
Krum Angelov <kbangelov@asteasolutions.com>
2026-02-06 13:41:02 +0200

    chore: fixed shift + tag bug artificially

--- packages/scratch-gui/src/hooks/use-action-menu-navigation.js
+++ packages/scratch-gui/src/hooks/use-action-menu-navigation.js
@@ -25,9 +25,15 @@ export default function useActionMenuNavigation (
     const containerRef = useRef(null);
     const buttonRef = useRef(null);
     const [isExpanded, setIsExpanded] = useState(false);
+    const skipNextBlurRef = useRef(false);
 
     useEffect(() => {
         if (!isExpanded) {
+            if (skipNextBlurRef.current) {
+                // Skip the blur this time
+                skipNextBlurRef.current = false;
+                return;
+            }
             // If menu is closed, blur any focused element to prevent keyboard events from affecting it
             if (document.activeElement !== document.body) {
                 document.activeElement.blur();
@@ -35,7 +41,7 @@ export default function useActionMenuNavigation (
             
             buttonRef?.current?.blur();
         }
-    }, [isExpanded]);
+    }, [isExpanded, buttonRef, skipNextBlurRef]);
 
     // Handle clicks/touches outside to close menu
     useEffect(() => {
@@ -102,16 +108,36 @@ export default function useActionMenuNavigation (
         switch (e.key) {
         case KEY.ARROW_DOWN:
             e.preventDefault();
-            e.stopPropagation();
             handleMove(1);
             break;
         case KEY.ARROW_UP:
             e.preventDefault();
-            e.stopPropagation();
             handleMove(-1);
             break;
         case KEY.TAB:
-            if (isExpanded) setIsExpanded(false);
+            setIsExpanded(false);
+            // A little bit hacky logic for shift + tab to move focus to previous element
+            if (e.shiftKey) {
+                e.preventDefault();
+                const focusables = Array.from(
+                    document.querySelectorAll('a, button, input, select, textarea, [tabindex]')
+                );
+
+                const filteredFocusables = focusables.filter(el => {
+                    // Skip disabled, hidden, or tabindex=-1
+                    if (el.disabled) return false;
+                    if (el.offsetParent === null) return false;
+                    const tabindex = el.getAttribute('tabindex');
+                    if (tabindex === '-1') return false;
+                    return true;
+                });
+                const currentIndex = filteredFocusables.indexOf(buttonRef.current);
+                if (currentIndex > 0) {
+                    filteredFocusables[currentIndex - 1].focus();
+                }
+
+                skipNextBlurRef.current = true;
+            }
             return;
         }
     }, [handleMove, isExpanded, setIsExpanded]);
